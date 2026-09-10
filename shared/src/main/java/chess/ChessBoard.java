3733cbfba1416a16541b2c1508889c1e3524b7e8
Gavin Bell <bgavin1@byu.edu>
2026-09-10 13:10:56 -0600

    added toString() for ChessBoard

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -55,6 +55,13 @@ public class ChessBoard {
         return Arrays.deepHashCode(squares);
     }
 
+    @Override
+    public String toString() {
+        return "ChessBoard{" +
+                "squares=" + Arrays.toString(squares) +
+                '}';
+    }
+
     /**
      * Sets the board to the default starting board
      * (How the game of chess normally starts)
