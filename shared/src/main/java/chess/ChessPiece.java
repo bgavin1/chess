27ef6cab249d55ca9b4d54554d7372e95214d29d
Gavin Bell <bgavin1@byu.edu>
2026-09-10 14:54:43 -0600

    Passed all other move tests except the pawn

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -83,19 +83,19 @@ public class ChessPiece {
         if (type == PieceType.KING) {
             return KingMoves(board, myPosition);
         } else if (type == PieceType.QUEEN) {
-            return null;
+            return QueenMoves(board, myPosition);
         } else if (type == PieceType.BISHOP) {
-            return null;
+            return BishopMoves(board, myPosition);
         } else if (type == PieceType.KNIGHT) {
-            return null;
+            return KnightMoves(board, myPosition);
         } else if (type == PieceType.ROOK) {
-            return null;
+            return RookMoves(board, myPosition);
         } else {
-            return null;
+            return PawnMoves(board, myPosition);
         }
 
     }
-    //will need to add logic for if there is a piece in the way
+
     private Collection<ChessMove> KingMoves(ChessBoard board, ChessPosition myPosition) {
         //what will be returned at the end
         Collection<ChessMove> MovesForKing = new ArrayList<>();
@@ -120,6 +120,162 @@ public class ChessPiece {
 
         return MovesForKing;
 
+    }
+
+    private Collection<ChessMove> BishopMoves(ChessBoard board, ChessPosition myPosition) {
+        //what will be returned at the end
+        Collection<ChessMove> MovesForBishop = new ArrayList<>();
+
+        //list of the coordinates of every direction that the bishop can move
+        int[][] directions = {{1, -1}, {1, 1}, {-1, -1}, {-1, 1}};
+        for (int[] direction : directions) {
+            int newRow = myPosition.getRow() + direction[0];
+            int newCol = myPosition.getColumn() + direction[1];
+            ChessPosition endPosition = new ChessPosition(newRow, newCol);
+            while (isOnBoard(endPosition)) {
+                //while the proposed endpoints are on the board
+                if (PieceOnSpot(board, endPosition) == null) {
+                    //if the spot is empty, add it and keep going that same direction
+                    MovesForBishop.add(new ChessMove(myPosition, endPosition, null));
+                    newRow += direction[0];
+                    newCol += direction[1];
+                    endPosition = new ChessPosition(newRow, newCol);
+                } else if (!IsMyTeam(board, myPosition, endPosition)) {
+                    //if the other team's piece is occupying the space, add the move and be done in that direction
+                    MovesForBishop.add(new ChessMove(myPosition, endPosition, null));
+                    break;
+                } else {
+                    break;
+                }
+            }
+
+
+        }
+
+
+        return MovesForBishop;
+    }
+
+    private Collection<ChessMove> KnightMoves(ChessBoard board, ChessPosition myPosition) {
+        //what will be returned at the end
+        Collection<ChessMove> MovesForKnight = new ArrayList<>();
+
+        //list of the coordinates of every direction that the king can move
+        int[][] directions = {{2, 1}, {2, -1}, {-2, 1}, {-2, -1}, {1, -2}, {-1, -2}, {1, 2}, {-1, 2}};
+
+        for (int[] direction : directions) {
+            int newRow = myPosition.getRow() + direction[0];
+            int newCol = myPosition.getColumn() + direction[1];
+            ChessPosition endPosition = new ChessPosition(newRow, newCol);
+            if (isOnBoard(endPosition)) {
+                //Determine if there is a piece occupying an available spot
+                if (PieceOnSpot(board, endPosition) == null) {
+                    MovesForKnight.add(new ChessMove(myPosition, endPosition, null));
+                } else if (!IsMyTeam(board, myPosition, endPosition)) {
+                    MovesForKnight.add(new ChessMove(myPosition, endPosition, null));
+                }
+            }
+        }
+
+        return MovesForKnight;
+    }
+
+    private Collection<ChessMove> QueenMoves(ChessBoard board, ChessPosition myPosition) {
+        //what will be returned at the end
+        Collection<ChessMove> MovesForQueen = new ArrayList<>();
+
+        //list of the coordinates of every direction that the queen can move
+        int[][] directions = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}, {1, -1}, {1, 1}, {-1, -1}, {-1, 1}};
+
+        for (int[] direction : directions) {
+            int newRow = myPosition.getRow() + direction[0];
+            int newCol = myPosition.getColumn() + direction[1];
+            ChessPosition endPosition = new ChessPosition(newRow, newCol);
+            while (isOnBoard(endPosition)) {
+                if (PieceOnSpot(board, endPosition) == null) {
+                    //if the spot is empty, add it and keep going that same direction
+                    MovesForQueen.add(new ChessMove(myPosition, endPosition, null));
+                    newRow += direction[0];
+                    newCol += direction[1];
+                    endPosition = new ChessPosition(newRow, newCol);
+                } else if (!IsMyTeam(board, myPosition, endPosition)) {
+                    //if the other team's piece is occupying the space, add the move and be done in that direction
+                    MovesForQueen.add(new ChessMove(myPosition, endPosition, null));
+                    break;
+                } else {
+                    break;
+                }
+
+            }
+
+        }
+
+        return MovesForQueen;
+    }
+
+    private Collection<ChessMove> PawnMoves(ChessBoard board, ChessPosition myPosition) {
+        //what will be returned at the end
+        Collection<ChessMove> MovesForPawn = new ArrayList<>();
+        ChessPiece myPiece = board.getPiece(myPosition);
+
+        int[][] captureDirections = {{1,1}, {1,-1}};
+
+        int direction;
+        int startingRow;
+        if (myPiece.getTeamColor() == ChessGame.TeamColor.WHITE) {
+            direction = 1;
+            startingRow = 2;
+        } else {
+            direction = -1;
+            startingRow = 7;
+        }
+        int moveTwo = myPosition.getRow() + (direction * 2);
+        int newRow = myPosition.getRow() + direction;
+        ChessPosition moveTwoEndPosition = new ChessPosition(moveTwo, myPosition.getColumn());
+        ChessPosition endPosition = new ChessPosition(newRow, myPosition.getColumn());
+
+        if (myPosition.getRow() == startingRow) {
+            MovesForPawn.add(new ChessMove(myPosition, endPosition, null));
+            MovesForPawn.add(new ChessMove(myPosition, moveTwoEndPosition, null));
+
+        }
+
+
+        return MovesForPawn;
+    }
+
+    private Collection<ChessMove> RookMoves(ChessBoard board, ChessPosition myPosition) {
+        //what will be returned at the end
+        Collection<ChessMove> MovesForRook = new ArrayList<>();
+
+        //list of the coordinates of every direction that the king can move
+        int[][] directions = {{1, 0}, {-1, 0}, {0, -1}, {0, 1}};
+
+        for (int[] direction : directions) {
+            int newRow = myPosition.getRow() + direction[0];
+            int newCol = myPosition.getColumn() + direction[1];
+            ChessPosition endPosition = new ChessPosition(newRow, newCol);
+            while (isOnBoard(endPosition)) {
+                if (PieceOnSpot(board, endPosition) == null) {
+                    //if the spot is empty, add it and keep going that same direction
+                    MovesForRook.add(new ChessMove(myPosition, endPosition, null));
+                    newRow += direction[0];
+                    newCol += direction[1];
+                    endPosition = new ChessPosition(newRow, newCol);
+                } else if (!IsMyTeam(board, myPosition, endPosition)) {
+                    //if the other team's piece is occupying the space, add the move and be done in that direction
+                    MovesForRook.add(new ChessMove(myPosition, endPosition, null));
+                    break;
+                } else {
+                    break;
+                }
+
+            }
+
+        }
+
+
+        return MovesForRook;
     }
 
     public boolean isOnBoard(ChessPosition endPosition) {
